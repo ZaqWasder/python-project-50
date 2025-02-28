ac165837b88d076651cdede6053b92cc53bf7d77
Artem Budarin <buldozer199@gmail.com>
2025-02-28 23:38:36 +0300

    added reading and parsing json-files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,22 @@
 import argparse
+import json
 
 
-def main():
+def parse_args():
     parser = argparse.ArgumentParser(
         prog='gendiff',
         description='Compares two configuration files and shows a difference.',
     )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', metavar='FORMAT', choices=['plain', 'json'], help='set format of output')
-    parser.parse_args()
+    return parser.parse_args()
 
-#    parser.add_argument('-h', '--help', help='show this help message and exit')
+
+def main():
+    args = parse_args()
+    first_file = json.load(open(args.first_file))
+    second_file = json.load(open(args.second_file))
 
 
 if __name__ == "__main__":
